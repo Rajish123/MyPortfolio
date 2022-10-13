3300505816d88dd2df5764c1c54367d82e527039
rajish <maharjanrajish101@gmail.com>
2022-10-13 15:49:14 +0545

    ContactView

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Contact
 
 # Create your views here.
 
 def home(request):
     return render(request,'portfolio/home.html')
+
+def about(request):
+    return render(request,'portfolio/about.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        concern = request.POST['concern']
+        obj = Contact(name=name,phone=phone,email=email,concern=concern)
+        obj.save()
+    return render(request,'portfolio/contact.html')
+ 
